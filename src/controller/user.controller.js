1153a3e20a1f618cff5fb011a9c54348ae5fab56
Prayag Vekariya <prayagvekariya026@gmail.com>
2025-08-04 00:04:47 +0530

    Implement user authentication with JWT and add subscription model

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -305,7 +305,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(new ApiResponse(200, user, "Cover image updated successfully"));
+    .json(new ApiResponse(200, user, "Cover image updated successfully."));
 });
 
 export {
